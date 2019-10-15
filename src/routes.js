a9a23ae3045c1e72340979d54bb8b70b01ac7c28
David Faria <davidfaria@gmail.com>
2019-10-15 17:35:49 -0300

    implementando modulo 03 gobarber

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+// Configs
+import multerConfig from './config/multer';
+
+// Middlewares
 import authMiddleware from './app/middlewares/auth';
 
+// Controllers
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import FileController from './app/controllers/FileController';
 
+// Variables
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', async (req, res) => {
   res.json({
@@ -19,5 +30,9 @@ routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 routes.put('/users/:id', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
